dfa6d389026ffb7b57531e03ddf992438cba11f1
augustoibarrola <augusto.t.ibarrola@gmail.com>
2020-08-13 16:38:45 -0400

    second installment

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,10 @@ class Student < ActiveRecord::Base
     def full_name
         self.first_name + " " + self.last_name
     end
+
+    def self.all_in_grade (grade)
+        self.all.select do |x|
+            x.grade_level == grade 
+        end
+    end
 end
