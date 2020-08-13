e7ad2f80f06803ae84d8fdc0013668108c110178
augustoibarrola <augusto.t.ibarrola@gmail.com>
2020-08-13 14:30:17 -0400

    began first deliverables

---
+++ db/seeds.rb
@@ -0,0 +1,9 @@
+Student.create(first_name: "augusto", last_name: "ibarrola", grade_level: "sixth")
+
+Student.create(first_name: "tiffany", last_name: "best", grade_level: "ninth")
+
+Student.create(first_name: "samantha", last_name: "lurio", grade_level: "eleventh")
+
+Teacher.create(last_name: "park", grade_level: "eleventh", years_of_experience: 8)
+
+Teacher.create(last_name: "mccarthy", grade_level: "bootcamp", years_of_experience: 100)
